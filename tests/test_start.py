46658093fbfc873733c0418c05fae7d90bb5df94
gibonson <jakubpalica@gmail.com>
2024-09-23 13:09:32 +0200

    test update

--- tests/test_start.py
+++ tests/test_start.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
 import time
 
 
@@ -11,7 +12,7 @@ driver = webdriver.Firefox()
 driver.get("http://127.0.0.1:5000/device_list")
 
 # Poczekaj chwilę na załadowanie strony
-time.sleep(3)
+time.sleep(1)
 
 # Znajdź przycisk po jego klasie (można też dodać więcej specyfikacji, jeśli potrzebne)
 add_device_button = driver.find_element(By.XPATH, "//button[normalize-space(text())='Add device']")
@@ -23,8 +24,21 @@ add_device_button.click()
 
 # Opcjonalnie: dalsze akcje po kliknięciu przycisku, np. weryfikacja, że formularz się rozwinął
 # Sprawdź, czy formularz o ID "form" jest widoczny
-form = driver.find_element(By.ID, "form1")
+form = driver.find_element(By.ID, "form")
 assert form.is_displayed()
 
+elem = driver.find_element(By.NAME, 'deviceName')  # Find the search box
+elem.send_keys('seleniumhq' + Keys.RETURN)
+
+
+# elem = driver.find_element(By.NAME, 'deviceIP')  # Find the search box
+# elem.send_keys('seleniumhq' + Keys.RETURN)
+
+# elem = driver.find_element(By.NAME, 'deviceStatus')  # Find the search box
+# elem.send_keys('seleniumhq' + Keys.RETURN)
+
+
+Select.select_by_value('Old')
+
 # Zamknij przeglądarkę
-driver.quit()
+# driver.quit()
